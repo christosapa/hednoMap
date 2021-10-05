36e768878e88750ccd1b1eb356c036564d215a9f
Christos Panos <christos.a.panos@gmail.com>
2021-10-05 12:45:42 +0300

    send J from API

--- epfMapApi/routes/locationsAPI.js
+++ epfMapApi/routes/locationsAPI.js
@@ -5,55 +5,16 @@ const cheerio = require("cheerio");
 const colors = require("colors");
 var _ = require('underscore');
 const NodeGeocoder = require('node-geocoder');
+const helper = require("../lib/helpers");
+//const locations = require("../lib/LocationsRequest")
 
-// value="9" > ΑΡΤΑΣ 
-// value="10"> ΑΤΤΙΚΗΣ
-// value="11" > ΑΧΑΙΑΣ
-// value="12" > ΒΟΙΩΤΙΑΣ
-// value="13" > ΓΡΕΒΕΝΩΝ
-// value="14" > ΔΡΑΜΑΣ
-// value="15" > ΔΩΔΕΚΑΝΗΣΟΥ
-// value="6" > ΕΒΡΟΥ
-// value="16" > ΕΥΒΟΙΑΣ
-// value="17" > ΕΥΡΥΤΑΝΙΑΣ
-// value="18" > ΖΑΚΥΝΘΟΥ
-// value="19" > ΗΛΕΙΑΣ
-// value="20" > ΗΜΑΘΙΑΣ
-// value="21" > ΗΡΑΚΛΕΙΟΥ
-// value="96" > ΘΕΣΠΡΩΤΙΑ
-// value="23" > ΘΕΣΣΑΛΟΝΙΚΗΣ
-// value="24" > ΙΩΑΝΝΙΝΩΝ
-// value="25" > ΚΑΒΑΛΑΣ
-// value="26" > ΚΑΡΔΙΤΣΑΣ
-// value="27" > ΚΑΣΤΟΡΙΑΣ
-// value="28" > ΚΕΡΚΥΡΑΣ
-// value="29" > ΚΕΦΑΛΛΟΝΙΑ
-// value="30" > ΚΙΛΚΙΣ
-// value="31" > ΚΟΖΑΝΗΣ
-// value="62" > ΚΟΡΙΝΘΙΑΣ
-// value="33" > ΚΥΚΛΑΔΩΝ
-// value="34" > ΛΑΚΩΝΙΑΣ
-// value="35" > ΛΑΡΙΣΑΣ
-// value="36" > ΛΑΣIΘΙΟΥ
-// value="37" > ΛΕΣΒΟΥ
-// value="38" > ΛΕΥΚΑΔΑΣ
-// value="39" > ΜΑΓΝΗΣΙΑΣ
-// value="40" > ΜΕΣΣΗΝΙΑΣ
-// value="41" > ΞΑΝΘΗΣ
-// value="43" > ΠΕΛΛΗΣ
-// value="44" > ΠΙΕΡΙΑΣ
-// value="45" > ΠΡΕΒΕΖΗΣ
-// value="46" > ΡΕΘΥΜΝΟΥ
-// value="47" > ΡΟΔΟΠΗΣ
-// value="48" > ΣΑΜΟΥ
-// value="49" > ΣΕΡΡΩΝ
-// value="50" > ΤΡΙΚΑΛΩΝ
-// value="51" > ΦΘΙΩΤΙΔΑΣ
-// value="52" > ΦΛΩΡΙΝΑΣ
-// value="53" > ΦΩΚΙΔΑΣ
-// value="54" > ΧΑΛΚΙΔΙΚΗΣ
-// value="55" > ΧΑΝΙΩΝ
-// value="56" > ΧΙΟΥ
+/* value="9" > ΑΡΤΑΣ value="10"> ΑΤΤΙΚΗΣ value="11" > ΑΧΑΙΑΣ value="12" > ΒΟΙΩΤΙΑΣ value="13" > ΓΡΕΒΕΝΩΝ value="14" > ΔΡΑΜΑΣ value="15" > ΔΩΔΕΚΑΝΗΣΟΥ
+   value="6" > ΕΒΡΟΥ value="16" > ΕΥΒΟΙΑΣ value="17" > ΕΥΡΥΤΑΝΙΑΣ value="18" > ΖΑΚΥΝΘΟΥ value="19" > ΗΛΕΙΑΣ value="20" > ΗΜΑΘΙΑΣ value="21" > ΗΡΑΚΛΕΙΟΥ
+   value="96" > ΘΕΣΠΡΩΤΙΑ value="23" > ΘΕΣΣΑΛΟΝΙΚΗΣ value="24" > ΙΩΑΝΝΙΝΩΝ value="25" > ΚΑΒΑΛΑΣ value="26" > ΚΑΡΔΙΤΣΑΣ value="27" > ΚΑΣΤΟΡΙΑΣ value="28" > ΚΕΡΚΥΡΑΣ
+   value="29" > ΚΕΦΑΛΛΟΝΙΑ value="30" > ΚΙΛΚΙΣ value="31" > ΚΟΖΑΝΗΣ value="62" > ΚΟΡΙΝΘΙΑΣ value="33" > ΚΥΚΛΑΔΩΝ value="34" > ΛΑΚΩΝΙΑΣ value="35" > ΛΑΡΙΣΑΣ
+   value="36" > ΛΑΣIΘΙΟΥ value="37" > ΛΕΣΒΟΥ value="38" > ΛΕΥΚΑΔΑΣ value="39" > ΜΑΓΝΗΣΙΑΣ value="40" > ΜΕΣΣΗΝΙΑΣ value="41" > ΞΑΝΘΗΣ
+   value="43" > ΠΕΛΛΗΣ value="44" > ΠΙΕΡΙΑΣ value="45" > ΠΡΕΒΕΖΗΣ value="46" > ΡΕΘΥΜΝΟΥ value="47" > ΡΟΔΟΠΗΣ value="48" > ΣΑΜΟΥ value="49" > ΣΕΡΡΩΝ
+   value="50" > ΤΡΙΚΑΛΩΝ value="51" > ΦΘΙΩΤΙΔΑΣ value="52" > ΦΛΩΡΙΝΑΣ value="53" > ΦΩΚΙΔΑΣ value="54" > ΧΑΛΚΙΔΙΚΗΣ value="55" > ΧΑΝΙΩΝ value="56" > ΧΙΟΥ */
 cityNumLocations = [67, 30, 96, 62].concat(_.range(6, 21), _.range(23, 31), _.range(33, 41), _.range(43, 56))
 
 const options = {
@@ -64,32 +25,10 @@ const options = {
 
 const geocoder = NodeGeocoder(options);
 
-function string_to_slug(str) {
-
-    str = str.replace(/^\s+|\s+$/g, '') // TRIM WHITESPACE AT BOTH ENDS.
-        .toLowerCase();            // CONVERT TO LOWERCASE
-
-    from = ["ου", "ΟΥ", "Ού", "ού", "αυ", "ΑΥ", "Αύ", "αύ", "ευ", "ΕΥ", "Εύ", "εύ", "α", "Α", "ά", "Ά", "β", "Β", "γ", "Γ", "δ", "Δ", "ε", "Ε", "έ", "Έ", "ζ", "Ζ", "η", "Η", "ή", "Ή", "θ", "Θ", "ι", "Ι", "ί", "Ί", "ϊ", "ΐ", "Ϊ", "κ", "Κ", "λ", "Λ", "μ", "Μ", "ν", "Ν", "ξ", "Ξ", "ο", "Ο", "ό", "Ό", "π", "Π", "ρ", "Ρ", "σ", "Σ", "ς", "τ", "Τ", "υ", "Υ", "ύ", "Ύ", "ϋ", "ΰ", "Ϋ", "φ", "Φ", "χ", "Χ", "ψ", "Ψ", "ω", "Ω", "ώ", "Ώ"];
-    to = ["ou", "ou", "ou", "ou", "au", "au", "au", "au", "eu", "eu", "eu", "eu", "a", "a", "a", "a", "b", "b", "g", "g", "d", "d", "e", "e", "e", "e", "z", "z", "i", "i", "i", "i", "th", "th", "i", "i", "i", "i", "i", "i", "i", "k", "k", "l", "l", "m", "m", "n", "n", "ks", "ks", "o", "o", "o", "o", "p", "p", "r", "r", "s", "s", "s", "t", "t", "y", "y", "y", "y", "y", "y", "y", "f", "f", "x", "x", "ps", "ps", "o", "o", "o", "o"];
-
-    for (var i = 0; i < from.length; i++) {
-        while (str.indexOf(from[i]) !== -1) {
-            str = str.replace(from[i], to[i]);    // CONVERT GREEK CHARACTERS TO LATIN LETTERS
-        }
-    }
-
-    // str = str.replace(/[^a-z0-9 -]/g, '') // REMOVE INVALID CHARS
-    //     .replace(/\s+/g, '-')        // COLLAPSE WHITESPACE AND REPLACE BY DASH - 
-    //     .replace(/-+/g, '-');        // COLLAPSE DASHES
-
-    return str;
-}
-
 coordsArray = [];
 
 for (cityNum of cityNumLocations) {
     for (let pageNum = 1; pageNum < 2; pageNum++) {
-        //console.log(pageNum)
         url = "https://siteapps.deddie.gr/Outages2Public/Home/OutagesPartial?page=" + pageNum + "&municipalityID=&prefectureID=" + cityNum
 
         axios
@@ -99,11 +38,9 @@ for (cityNum of cityNumLocations) {
 
                 $("body > div > div > table > tbody > tr").each((index, element) => {
                     // TODO find duplicate entries
-                    geocoder.geocode(string_to_slug($($(element).find("td")[2]).text()))
+                    geocoder.geocode(helper.string_to_slug($($(element).find("td")[2]).text()))
                         .then(function (res) {
-                            // console.log(string_to_slug($($(element).find("td")[2]).text()))
-                            // console.log(res[0].latitude);
-                            coordsArray = coordsArray + "[" + String(res[0].latitude) + ',' + String(res[0].longitude) + "]\n";
+                            coordsArray.push({ latitude: res[0].latitude, longitude: res[0].longitude })
                         })
                         .catch(function (err) {
                             console.log(err);
@@ -114,8 +51,17 @@ for (cityNum of cityNumLocations) {
     }
 }
 
-router.get("/", function (req, res, next) {
-    res.send(coordsArray);
-});
+// async function main() {
 
+//     var locationsToSend = await locations.faultLocationsToCoords()
+//     router.get("/", function (req, res, next) {
+//         res.send(JSON.stringify(locationsToSend));
+//     });
+// }
+
+// main();
+
+    router.get("/", function (req, res, next) {
+        res.send(JSON.stringify(coordsArray));
+    });
 module.exports = router;
--- epfMapClient/src/components/Maps.js
+++ epfMapClient/src/components/Maps.js
@@ -8,47 +8,33 @@ const mapStyles = {
 
 class Maps extends Component {
 
+    // TODO is this function needed?
     seperateCoords(Locations) {
-        let coords = [];
-        for (let i=0; i < Locations.split('\n').length - 1; i++) {
-            coords[i] = [];
-            coords[i][0] = [parseFloat(Locations.split('\n')[i].split(',')[0].replace('[', ''))]
-            coords[i][1] = [parseFloat(Locations.split('\n')[i].split(',')[1].replace(']', ''))]
-        }
+        let coords = Locations;
         return coords
     }
 
-
     displayMarkers(coords) {
-        console.log(coords)
         return <Marker position={{
-            lat: coords[0],
-            lng: coords[1]
+            lat: coords.latitude,
+            lng: coords.longitude
         }} />
-
     }
 
     render() {
         return (
-            // for debugging..
-            // <div>
-            //     <p>{this.props.markerLocation.split('\n')[0].split(',')[0].replace('[','')}</p>
-            //     <p>{this.props.markerLocation.split('\n')[0].split(',')[1].replace(']','')}</p>
-            // </div>
             <Map
                 google={this.props.google}
                 zoom={7}
                 style={mapStyles}
                 initialCenter={{ lat: 38.6, lng: 24.2 }}
             >
-                {this.seperateCoords(this.props.markerLocation).map((object, i) => this.displayMarkers(object))}
+                {this.seperateCoords(JSON.parse(this.props.markerLocation)).map((object, i) => this.displayMarkers(object))}
             </Map>
         )
 
     }
 }
-
-
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyDWbxY8wOy9rYue9YsyJAVO9VpYFqkVSZ8'
