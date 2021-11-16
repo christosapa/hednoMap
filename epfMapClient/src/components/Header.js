5d0c5085f0bada1253cd4a4641d0cba638ac6195
Christos Panos <christos.a.panos@gmail.com>
2021-11-16 18:16:07 +0200

    responsive layout for mobile browser

--- epfMapClient/src/components/Header.js
+++ epfMapClient/src/components/Header.js
@@ -1,26 +1,68 @@
 import React from 'react'
-import mainLogo from '../assets/logo.png';
+import { Component } from 'react';
+import mainLogo from '../assets/logo_tr.png';
+import mainLogo192 from '../assets/logo_tr192.png';
 import './Header.css'
 
 function reloadPage() {
     window.location.reload()
 }
 
-const Header = () => {
-    return (
-        <nav>
-            <div className='div-header'>
-                <div className='div-logo'>
-                    <img onClick={() => reloadPage()}
-                        src={mainLogo} alt='logo' />
-                </div>
-                <div className='div-name'>
-                    <h1>Live and planned power outages.</h1>
-                </div>
-            </div>
-        </nav>
-    )
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+
+    render() {
+        const { width } = this.state;
+        const isMobile = (width <= 600);
+        if (isMobile) {
+            return (
+                <nav>
+                    <div className='.div-header'>
+                        <div className='div-logo'>
+                            <img onClick={() => reloadPage()}
+                                src={mainLogo192} alt='logo' />
+                        </div>
+                        <div className='div-name'>
+                            <h1>Live and planned power outages.</h1>
+                        </div>
+                    </div>
+                </nav >
+            )
+        }
+        else {
+            return (
+                <nav>
+                    <div className='div-header'>
+                        <div className='div-logo'>
+                            <img onClick={() => reloadPage()}
+                                src={mainLogo} alt='logo' />
+                        </div>
+                        <div className='div-name'>
+                            <h1>Live and planned power outages.</h1>
+                        </div>
+                    </div>
+                </nav >
+            )
+        }
+    }
 }
 
-
-export default Header
+export default Header;
