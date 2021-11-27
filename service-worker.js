77dee77083649ee27846cf1dc7f4d521eb595f8f
Christos Panos <christos.a.panos@gmail.com>
2021-11-27 19:35:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hednoMap/precache-manifest.8526af046ccc389814b3dbb7d31768f9.js"
+  "/hednoMap/precache-manifest.006d4d20ba48b4438126efc072f79936.js"
 );
 
 self.addEventListener('message', (event) => {
