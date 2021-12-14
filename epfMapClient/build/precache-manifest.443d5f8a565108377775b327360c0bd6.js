daf4aba42e92cbef3450987f7d459381ffb1a891
Christos Panos <christos.a.panos@gmail.com>
2021-12-14 18:40:02 +0200

    build

---
+++ epfMapClient/build/precache-manifest.443d5f8a565108377775b327360c0bd6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101db874e6ca9c81c900fbe061168b5f",
+    "url": "/hednoMap/index.html"
+  },
+  {
+    "revision": "89cb9d1a9f32b75dcda6",
+    "url": "/hednoMap/static/css/2.68584d97.chunk.css"
+  },
+  {
+    "revision": "1b6ce6eb205ee7163fd9",
+    "url": "/hednoMap/static/css/main.b2c745d3.chunk.css"
+  },
+  {
+    "revision": "89cb9d1a9f32b75dcda6",
+    "url": "/hednoMap/static/js/2.381eb298.chunk.js"
+  },
+  {
+    "revision": "22846d55ba0af4676140da083bb080fb",
+    "url": "/hednoMap/static/js/2.381eb298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcce8e306044e8e2b7a",
+    "url": "/hednoMap/static/js/3.1ba75d08.chunk.js"
+  },
+  {
+    "revision": "1b6ce6eb205ee7163fd9",
+    "url": "/hednoMap/static/js/main.0e995681.chunk.js"
+  },
+  {
+    "revision": "07ad52e8a88a6185929c",
+    "url": "/hednoMap/static/js/runtime-main.7f9c7cc2.js"
+  },
+  {
+    "revision": "595df5820c475d9bbdf527c4893a96c7",
+    "url": "/hednoMap/static/media/logo_tr.595df582.png"
+  },
+  {
+    "revision": "5e19bf2777a0b54198a16a8a3671cb61",
+    "url": "/hednoMap/static/media/myLocation.5e19bf27.svg"
+  }
+]);
