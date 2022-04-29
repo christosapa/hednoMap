aa590e64d1fec453cb59828121c3e1fee2fb32d6
Christos Panos <christos.a.panos@gmail.com>
2022-04-29 17:41:13 +0300

    added mongoDB connection in server file

--- epfMapBackend/server.js
+++ epfMapBackend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -8,7 +9,12 @@ const errorHandler = require('./middleware/errorHandler');
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
 const credentials = require('./middleware/credentials');
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn')
 const PORT = process.env.PORT || 3500;
+
+// connect to MongoDB
+connectDB();
 
 // custom middleware logger
 app.use(logger)
@@ -40,7 +46,6 @@ app.use('/refresh', require('./routes/refresh'))
 app.use('/logout', require('./routes/logout'))
 
 app.use(verifyJWT)
-app.use('/employees', require('./routes/api/employees'))
 
 app.all('*', (req, res) => {
     res.status(404);
@@ -55,4 +60,7 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
