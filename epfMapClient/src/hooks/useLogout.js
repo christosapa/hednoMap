45c4f626e83c09c948c2ef21796b5bf41c481b8a
Christos Panos <christos.a.panos@gmail.com>
2022-05-13 19:46:38 +0300

    logout hook

--- epfMapClient/src/hooks/useLogout.js
+++ epfMapClient/src/hooks/useLogout.js
@@ -2,15 +2,15 @@ import axios from "../api/axios";
 import useAuth from "./useAuth";
 
 const useLogout = () => {
-    
+
     const { setAuth } = useAuth();
 
     const logout = async () => {
         setAuth({});
         try {
-            const response = await axios('/logout', {
-                withCredentials: true
-            });
+            const response = await axios('/logout',
+                { withCredentials: true }
+            );
         } catch (err) {
             console.error(err);
         }
