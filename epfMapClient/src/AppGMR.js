fe09145bf4bb1e915e300776fd47f67979a23101
Christos Panos <christos.a.panos@gmail.com>
2022-05-13 19:45:53 +0300

    added welcome route for email verification

--- epfMapClient/src/AppGMR.js
+++ epfMapClient/src/AppGMR.js
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from './components/Layouts';
 import { Routes, Route } from 'react-router-dom';
 import Maps from './components/MapsGMR'
+import Welcome from './components/Welcome'
 // import Login from './components/Login';
 // import Signup from './components/Signup';
 import RequireAuth from './components/RequireAuth';
@@ -14,6 +15,7 @@ export default function AppGMR() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/hednoMap' element={<Maps />} />
+          <Route path='/hednoMap/confirm/:confirmationCode' element={<Welcome />} />
           {/* <Route path='/hednoMap/login' element={<Login />} /> */}
           {/* <Route path='/hednoMap/signup' element={<Signup />} /> */}
           <Route element={<RequireAuth />}>
