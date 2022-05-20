9e7ba4f9860f33bbb3804070e44c4e396392f1b8
Christos Panos <christos.a.panos@gmail.com>
2022-05-20 18:41:05 +0300

    fix github origin

--- epfMapBackend/config/allowedOrigins.js
+++ epfMapBackend/config/allowedOrigins.js
@@ -1,8 +1,7 @@
 const allowedOrigins = [
-    'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://christosapa.github.io/hednoMap'
+    'https://christosapa.github.io'
 ];
 
 module.exports = allowedOrigins;
